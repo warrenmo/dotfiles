0f50fa42a5e337e9a92a983c5bb8b99ede401331
Warren Mo <wmowkm@gmail.com>
2024-10-16 21:05:42 -0400

    nvim packer, telescope, ff, zenburn

---
+++ nvim/lua/core/plugins.lua
@@ -0,0 +1,33 @@
+-- BEGIN packer (bootstrapped)
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+return require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+  -- or                            , branch = '0.1.x',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+  use {
+    "phha/zenburn.nvim",
+    config = function() require("zenburn").setup() end
+  }
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
+-- END packer
