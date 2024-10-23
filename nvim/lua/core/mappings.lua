01c0146b9af6cbfee558909e9870588035519fd1
Warren Mo <wmowkm@gmail.com>
2024-10-22 21:35:36 -0500

    pane navigation

--- nvim/lua/core/mappings.lua
+++ nvim/lua/core/mappings.lua
@@ -1,2 +1,7 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "-", vim.cmd.Ex)
+
+vim.keymap.set('n', '<C-h>', '<C-w>h', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-j>', '<C-w>j', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-k>', '<C-w>k', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-l>', '<C-w>l', { noremap = true, silent = true })
